93b45b62edcf414a06a604438111647f167ad87e
Max Zhan <max.zhan@jpl.nasa.gov>
2022-08-18 20:09:02 -0700

    Updated the commit to the submodule.

--- debug/execute.sh
+++ debug/execute.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -xuo pipefail
 
-subset_py="conda run --no-capture-output -n gedi_subset /home/jovyan/maap-documentation-examples/gedi-subset/subset.py"
+subset_py="conda run --no-capture-output -n gedi_subset /home/jovyan/maap-documentation-examples/gedi-subset/src/subset.py"
 
 aoi="aoi.geojson"
 columns="agbd, agbd_se, l2_quality_flag, sensitivity, sensitivity_a2"
