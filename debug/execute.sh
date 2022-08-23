2714cc961c7fdb81bd42852a78192dce3c17afb0
jplzhan <100379221+jplzhan@users.noreply.github.com>
2022-08-23 13:46:31 -0700

    Update execute.sh

--- debug/execute.sh
+++ debug/execute.sh
@@ -4,8 +4,8 @@ set -xuo pipefail
 subset_py="conda run --no-capture-output -n gedi_subset /home/jovyan/maap-documentation-examples/gedi-subset/src/subset.py"
 
 aoi="aoi.geojson"
-columns="agbd, agbd_se, l2_quality_flag, sensitivity, sensitivity_a2"
+columns="agbd, agbd_se, l2_quality_flag, l4_quality_flag, sensitivity, sensitivity_a2, lon_lowestmode, lat_lowestmode"
 query="l2_quality_flag == 1 and l4_quality_flag == 1 and sensitivity > 0.95 and sensitivity_a2 > 0.95"
-limit="10000"
+limit="5"
 
-$subset_py --verbose --aoi $aoi --columns "$columns" --query "$query" --limit $limit
+$subset_py --cmr-host cmr.earthdata.nasa.gov --verbose --aoi $aoi --columns "$columns" --query "$query" --limit $limit
